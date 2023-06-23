deaa69d871404bc139850e43aadf9cd866cb5b09
Sergey Boltyshov <boltyshovs@yandex.ru>
2023-06-23 14:07:48 +0300

    over commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,70 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
-
+    private Map<String, List<PageEntry>> map = new HashMap<>();
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        for (File pdf : pdfsDir.listFiles()) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            int NumberOfPages = doc.getNumberOfPages();
+            for (int i = 1;i <= NumberOfPages; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+                for (var word : words) { // перебираем слова
+                    if ((word.isEmpty())) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                for (Map.Entry<String, Integer> entry : freqs.entrySet()) {
+                    var word = entry.getKey();
+                    var count = entry.getValue();
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, count);
+
+                    if (map.containsKey(word)) {
+                        map.get(word).add(pageEntry);
+                    } else {
+                        map.computeIfAbsent(word, w -> new ArrayList<>()).add(pageEntry);
+                    }
+                }
+            }
+        }
     }
 
     @Override
-    public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+    public List<PageEntry> search(String words) {
+
+        String[] request = words.toLowerCase().split("\\P{IsAlphabetic}+");
+        List<PageEntry> tempList = new ArrayList<>();
+        List<PageEntry> result = new ArrayList<>();
+
+        for (String newWord : request) {
+            if (map.get(newWord) != null) {
+                tempList.addAll(map.get(newWord));
+            }
+        }
+        Map<String, Map<Integer, Integer>> numberAndQuantity = new HashMap<>();
+        for (PageEntry pageEntry : tempList) {
+            numberAndQuantity.computeIfAbsent(pageEntry.getPdfName(), key -> new HashMap<>())
+                    .merge(pageEntry.getPage(), pageEntry.getCount(), Integer::sum);
+        }
+
+        numberAndQuantity.forEach((key, value) -> {
+            for (var tempPage : value.entrySet()) {
+                result.add(new PageEntry(key, tempPage.getKey(), tempPage.getValue()));
+            }
+        });
+        Collections.sort(result);
+        return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,31 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+        System.out.println(gson.toJson(engine.search("бизнес")));
+
+        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+            while (true) { // в цикле(!) принимаем подключения
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())){
+                        String word = in.readLine();
+                        out.println(gson.toJson(engine.search(word)));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
     }
